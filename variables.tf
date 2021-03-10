816121b0053eb0a269f37fb83311655cb1a3d589
nana808-git <nana.devtestacct@gmail.com>
2021-03-10 11:04:05 -0600

    ok

--- variables.tf
+++ variables.tf
@@ -21,15 +21,15 @@ variable "vpc_id" {
   description = "vpc for provisioning resources"
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "public subnet array (length>=2)"
-}
+#variable "public_subnets" {
+#  type        = list(string)
+#  description = "public subnet array (length>=2)"
+#}
 
-variable "private_subnets" {
-  type        = list(string)
-  description = "public subnet array (length>=2)"
-}
+#variable "private_subnets" {
+#  type        = list(string)
+#  description = "public subnet array (length>=2)"
+#}
 
 variable "alb_port" {
   type        = string
@@ -148,14 +148,6 @@ variable "domain_name" {
 variable "codestar_connector_credentials" {
   type = string
   default = ""
-}
-
-variable "app" {
-  type = "map"
-  default = {
-    name = ""
-    env  = ""
-  }
 }
 
 variable "availability_zones" {
