223ec37633a1db7e03f7f9e4fbd6badf09db5b5c
nana808-git <nana.devtestacct@gmail.com>
2021-03-10 12:25:07 -0600

    ok

--- terraform/environments/Dev/variables.tf
+++ terraform/environments/Dev/variables.tf
@@ -42,17 +42,13 @@ variable "network" {
 
 variable "public_subnets" {
   type = "string"
-  default = ["10.100.80.0/22", "10.100.84.0/22"]
-    #publicAz1  = "10.100.80.0/22"
-    #publicAz2  = "10.100.84.0/22"
+  default = "["10.100.80.0/22", "10.100.84.0/22"]"
   }
 }
 
 variable "private_subnets" {
   type = "string"
-  default = ["10.100.88.0/22", "10.100.92.0/22"]
-    #privateAz1 = "10.100.88.0/22"
-    #privateAz2 = "10.100.92.0/22"
+  default = "["10.100.88.0/22", "10.100.92.0/22"]"
   }
 }
 
