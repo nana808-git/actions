b3b705ee763fae3e1ffa2a8a0891266b77832bb3
nana808-git <nana.devtestacct@gmail.com>
2021-03-10 13:00:24 -0600

    ok

--- terraform/environments/Dev/main.tf
+++ terraform/environments/Dev/main.tf
@@ -50,9 +50,9 @@ module "ecs-pipeline" {
   vpc_id         = var.vpc_id
   #public_subnets = module.vpc.public_subnets
   #private_subnets = module.vpc.private_subnets
-  network          = var.network
-  #public_subnets = var.public_subnets
-  #private_subnets = var.private_subnets
+  #network          = var.network
+  public_subnets = var.network
+  private_subnets = var.network
 
   cluster_name        = local.application_name
   app_repository_name = local.application_name
