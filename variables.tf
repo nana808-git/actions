f3576c9213b63617884eebfd124597fdaec8a00f
nana808-git <nana.devtestacct@gmail.com>
2021-03-10 08:59:27 -0600

    ok

--- variables.tf
+++ variables.tf
@@ -147,99 +147,82 @@ variable "domain_name" {
 
 variable "codestar_connector_credentials" {
   type = string
-  #default = "arn:aws:codestar-connections:us-west-1:710789462061:connection/024d34e3-7643-4ffe-ab6a-93053546f46f"
   default = ""
 }
 
 variable "app" {
   type = "map"
   default = {
-    name = "cc"
-    env  = "dev"
+    name = ""
+    env  = ""
   }
 }
 
 variable "availability_zones" {
   type = "list"
   default = [
-    "us-east-1a", 
-    "us-east-1b",
+    "", 
+    "",
   ]
 }
 
 variable "region" {
   type = "string"
-  default = "us-east-1"
+  default = ""
 }
 
 variable "network" {
   type = "map"
   default = {
-    cidr       = "10.100.80.0/20"
-    publicAz1  = "10.100.80.0/22"
-    publicAz2  = "10.100.84.0/22"
-    privateAz1 = "10.100.88.0/22"
-    privateAz2 = "10.100.92.0/22"
+    cidr       = ""
+    publicAz1  = ""
+    publicAz2  = ""
+    privateAz1 = ""
+    privateAz2 = ""
   }
 }
 
 variable "ami_id" {
   type = "string"
-  default = "ami-05170f6170a3069ac"
+  default = ""
 }
 
 variable "certificate_arn" {
   type = "string"
-  default = "arn:aws:acm:us-east-1:667736119737:certificate/8a4cdeec-e44c-42c0-b4ce-c1d2dc12f657"
+  default = ""
 }
 
 variable "node_volume_size" {
   type = "string"
-  default = "10"
+  default = ""
 }
 
 variable "node_instance_type" {
   type = "string"
-  default = "t2.large"
+  default = ""
 }
 
 variable "nosql_volume_size" {
   type = "string"
-  default = "10"
+  default = ""
 }
 
 variable "nosql_instance_type" {
   type = "string"
-  default = "t2.large"
+  default = ""
 }
 
 variable "nat_count" {
   type = "string"
-  default = "1"
+  default = ""
 }
 
 variable "escluster_instance_count" {
   type = "string"
-  default = "2"
+  default = ""
 }
 
 variable "escluster_instance_type" {
   type = "string"
-  default = "t2.medium.elasticsearch"
+  default = ""
 }
-
-output "availability_zones" { value = "${var.availability_zones}" }
-output "certificate_arn" { value = "${var.certificate_arn}" }
-output "region" { value = "${var.region}" }
-output "network" { value = "${var.network}" }
-output "ami_id" { value = "${var.ami_id}" }
-output "app" { value = "${var.app}" }
-output "node_volume_size" { value = "${var.node_volume_size}" }
-output "node_instance_type" { value = "${var.node_instance_type}" }
-output "nosql_volume_size" { value = "${var.nosql_volume_size}" }
-output "nosql_instance_type" { value = "${var.nosql_instance_type}" }
-output "nat_count" { value = "${var.nat_count}" }
-output "escluster_instance_type" { value = "${var.escluster_instance_type}" }
-output "escluster_instance_count" { value = "${var.escluster_instance_count}" }
-
-
