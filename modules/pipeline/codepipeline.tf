2164a02dd0d5bb828c12d04ad18e23d282f09f9c
nana808-git <nana.devtestacct@gmail.com>
2021-03-12 09:21:47 -0600

    ok

--- modules/pipeline/codepipeline.tf
+++ modules/pipeline/codepipeline.tf
@@ -9,42 +9,59 @@ resource "aws_codepipeline" "pipeline" {
     location = "${aws_s3_bucket.source.bucket}"
     type     = "S3"
   }
-  
-    stage {
-      name = "Source"
-      action{
-        name = "Source"
-        category = "Source"
-        owner = "AWS"
-        provider = "CodeStarSourceConnection"
-        version = "1"
-        output_artifacts = ["source"]
-        configuration = {
-          FullRepositoryId = "${lookup(var.git_repository,"FullRepositoryId")}"
-          BranchName   = "${lookup(var.git_repository,"BranchName")}"
-          ConnectionArn = "${lookup(var.git_repository,"ConnectionArn")}"
-          OutputArtifactFormat = "CODE_ZIP"
-        }
-      }
-    }
 
   stage {
-    name = "Build"
-
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      version          = "1"
-      input_artifacts  = ["source"]
-      output_artifacts = ["BuildOutput"]
-
+    name = "Source"
+    action{
+      name = "Source"
+      category = "Source"
+      owner = "AWS"
+      provider = "ECR"
+      version = "1"
+      output_artifacts = ["source"]
       configuration = {
-        ProjectName = "${var.cluster_name}-${var.environment}-codebuild"
+        #RepositoryName = "${aws_ecr_repository.this.name}"
+        RepositoryName = "${app_repository_name}"
+        ImageTag       = "latest"
       }
     }
   }
+  
+#  stage {
+#    name = "Source"
+#    action{
+#      name = "Source"
+#      category = "Source"
+#      owner = "AWS"
+#      provider = "CodeStarSourceConnection"
+#      version = "1"
+#      output_artifacts = ["source"]
+#      configuration = {
+#        FullRepositoryId = "${lookup(var.git_repository,"FullRepositoryId")}"
+#        BranchName   = "${lookup(var.git_repository,"BranchName")}"
+#        ConnectionArn = "${lookup(var.git_repository,"ConnectionArn")}"
+#        OutputArtifactFormat = "CODE_ZIP"
+#      }
+#    }
+#  }
+
+#  stage {
+#    name = "Build"
+
+#    action {
+#      name             = "Build"
+#      category         = "Build"
+#      owner            = "AWS"
+#      provider         = "CodeBuild"
+#      version          = "1"
+#      input_artifacts  = ["source"]
+#      output_artifacts = ["BuildOutput"]
+#
+#      configuration = {
+#        ProjectName = "${var.cluster_name}-${var.environment}-codebuild"
+#      }
+#    }
+#  }
 
   stage {
     name = "Staging"
@@ -82,22 +99,22 @@ resource "aws_codepipeline" "pipeline" {
   }
 }
 
-locals {
-  webhook_secret = "super-secret"
-}
+#locals {
+#  webhook_secret = "super-secret"
+#}
 
-resource "aws_codepipeline_webhook" "pipeline" {
-  name            = "test-webhook-github-pipeline"
-  authentication  = "GITHUB_HMAC"
-  target_action   = "Source"
-  target_pipeline = aws_codepipeline.pipeline.name
+#resource "aws_codepipeline_webhook" "pipeline" {
+#  name            = "test-webhook-github-pipeline"
+#  authentication  = "GITHUB_HMAC"
+#  target_action   = "Source"
+#  target_pipeline = aws_codepipeline.pipeline.name
 
-  authentication_configuration {
-    secret_token = local.webhook_secret
-  }
+#  authentication_configuration {
+#    secret_token = local.webhook_secret
+#  }
 
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}"
-  }
-}
+#  filter {
+#    json_path    = "$.ref"
+#    match_equals = "refs/heads/{Branch}"
+#  }
+#}
