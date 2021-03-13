e622f3ea21c220292b472232941e71e4f31e6daa
nana808-git <nana.devtestacct@gmail.com>
2021-03-13 13:27:38 -0600

    ok

--- modules/pipeline/iam.tf
+++ modules/pipeline/iam.tf
@@ -105,7 +105,7 @@ data "template_file" "events" {
 data "template_file" "ecr_event" {
   template = file("${path.module}/templates/policies/ecr-source-event.json")
   vars = {
-    ecr_repository_name = var.repository_name
+    ecr_repository_name = module.ecs.repository_name
   }
 }
 
