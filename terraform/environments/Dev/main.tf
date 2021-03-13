d1230cf822b0182751975bb549f88ef0b6c1794b
nana808-git <nana.devtestacct@gmail.com>
2021-03-13 17:07:07 -0600

    ok

--- terraform/environments/Dev/main.tf
+++ terraform/environments/Dev/main.tf
@@ -47,6 +47,7 @@ module "ecs-pipeline" {
   app_repository_name = local.application_name
   container_name      = local.application_name
   environment         = local.environment
+  repository_name     = module.ecs.repository_name
 
   alb_port         = "80"
   container_port   = "3000"
