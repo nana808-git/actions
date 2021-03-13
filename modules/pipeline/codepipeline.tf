b8c71c16408e8675db664f9100a49e9b506bab21
nana808-git <nana.devtestacct@gmail.com>
2021-03-12 19:04:47 -0600

    ok

--- modules/pipeline/codepipeline.tf
+++ modules/pipeline/codepipeline.tf
@@ -25,21 +25,6 @@ resource "aws_codepipeline" "pipeline" {
         ImageTag       = "latest"
       }
     }
-    {
-      name = "Source"
-      category = "Source"
-      owner = "AWS"
-      provider = "CodeStarSourceConnection"
-      version = "1"
-      output_artifacts = ["source"]
-      configuration = {
-        FullRepositoryId = "${lookup(var.git_repository,"FullRepositoryId")}"
-        BranchName   = "${lookup(var.git_repository,"BranchName")}"
-        ConnectionArn = "${lookup(var.git_repository,"ConnectionArn")}"
-        OutputArtifactFormat = "CODE_ZIP"
-      }
-    }
-  }
 
 #  stage {
 #    name = "Source"
@@ -61,7 +46,6 @@ resource "aws_codepipeline" "pipeline" {
 
   stage {
     name = "Build"
-
     action {
       name             = "Build"
       category         = "Build"
@@ -69,7 +53,8 @@ resource "aws_codepipeline" "pipeline" {
       provider         = "CodeBuild"
       version          = "1"
       input_artifacts  = ["source"]
-      output_artifacts = ["BuildOutput"]
+      #output_artifacts = ["BuildOutput"]
+      output_artifacts = ["imagedefinitions"]
 
       configuration = {
         ProjectName = "${var.cluster_name}-${var.environment}-codebuild"
@@ -101,7 +86,7 @@ resource "aws_codepipeline" "pipeline" {
       category        = "Deploy"
       owner           = "AWS"
       provider        = "ECS"
-      input_artifacts = ["BuildOutput"]
+      input_artifacts = ["imagedefinitions"]
       version         = "1"
 
       configuration = {
@@ -113,22 +98,22 @@ resource "aws_codepipeline" "pipeline" {
   }
 }
 
-locals {
-  webhook_secret = "super-secret"
-}
+#locals {
+#  webhook_secret = "super-secret"
+#}
 
-resource "aws_codepipeline_webhook" "pipeline" {
-  name            = "test-webhook-github-pipeline"
-  authentication  = "GITHUB_HMAC"
-  target_action   = "Source"
-  target_pipeline = aws_codepipeline.pipeline.name
+#resource "aws_codepipeline_webhook" "pipeline" {
+#  name            = "test-webhook-github-pipeline"
+#  authentication  = "GITHUB_HMAC"
+#  target_action   = "Source"
+#  target_pipeline = aws_codepipeline.pipeline.name
 
-  authentication_configuration {
-    secret_token = local.webhook_secret
-  }
+#  authentication_configuration {
+#    secret_token = local.webhook_secret
+#  }
 
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}"
-  }
-}
+#  filter {
+#    json_path    = "$.ref"
+#    match_equals = "refs/heads/{Branch}"
+#  }
+#}
