977b000b977045c8dfb8ef2e1f37e620968d1b45
nana808-git <nana.devtestacct@gmail.com>
2021-03-15 06:56:20 -0500

    ok

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -72,31 +72,35 @@ resource "aws_alb_listener" "web_app_ssl" {
   }
 }
 
-data "aws_route53_zone" "selected" {
-  count = local.can_domain ? 1 : 0
 
-  name = "${var.domain_name}."
-  #name = "isvc.tech."
-}
+
+
+
+#data "aws_route53_zone" "selected" {
+#  count = local.can_domain ? 1 : 0
+
+#  name = "${var.domain_name}."
+#  #name = "isvc.tech."
+#}
 
 # 
-resource "aws_route53_record" "alb_alias" {
-  count = local.can_domain ? 1 : 0
+#resource "aws_route53_record" "alb_alias" {
+#  count = local.can_domain ? 1 : 0
 
-  name    = var.domain_name
-  zone_id = data.aws_route53_zone.selected[0].zone_id
-  type    = "A"
+#  name    = var.domain_name
+#  zone_id = data.aws_route53_zone.selected[0].zone_id
+#  type    = "A"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#  lifecycle {
+#    create_before_destroy = true
+#  }
 
-  alias {
-    name                   = aws_alb.app_alb.dns_name
-    zone_id                = aws_alb.app_alb.zone_id
-    evaluate_target_health = true
-  }
-}
+#  alias {
+#    name                   = aws_alb.app_alb.dns_name
+#    zone_id                = aws_alb.app_alb.zone_id
+#    evaluate_target_health = true
+#  }
+#}
 
 resource "aws_alb_listener" "web_app_http" {
   count = local.is_only_http ? 1 : 0
@@ -137,5 +141,166 @@ resource "aws_lb_listener" "http_redirect_https" {
       status_code = "HTTP_301"
     }
   }
+}
+
+
+
+
+
+
+
+
+
+## Route 53
+# Provides details about the zone
+data "aws_route53_zone" "main" {
+  name         = "${var.domain_name}."
+  private_zone = false
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.bucket.bucket_regional_domain_name}"
+    origin_id   = "s3"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.OAI.cloudfront_access_identity_path}"
+    }
+  }
+#  origin {
+#    domain_name = replace(aws_api_gateway_deployment.deployment.invoke_url, "/^https?://([^/]*).*/", "$1")
+#    origin_id   = "apigw"
+
+#    custom_origin_config {
+#      http_port              = 80
+#      https_port             = 443
+#      origin_protocol_policy = "https-only"
+#      origin_ssl_protocols   = ["TLSv1.2"]
+#    }
+#  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    #viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "apigw"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    #viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "allow-all"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "domain" {
+  value = "${aws_cloudfront_distribution.distribution.domain_name}"
+}
+
+# Creates the DNS record to point on the CloudFront distribution ID that handles the redirection website
+resource "aws_route53_record" "website_cdn_redirect_record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${var.cluster_name}-${var.environment}.${var.domain_name}."
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+# -------------- Origin resources --------------
+
+# s3 origin
+resource "aws_s3_bucket" "bucket" {
+  acl    = "public-read"
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  tags = {
+    Name = "${var.cluster_name}-${var.environment}-bucket"
+  }  
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "${aws_s3_bucket.bucket.bucket}"
+  key    = "index.html"
+
+  content      = <<EOF
+<script>
+const call = (path) => async () => {
+	[...document.querySelectorAll("button")].forEach((b) => b.disabled = true);
+	const result = await (await fetch(path)).text();
+	document.querySelector("#result").innerText = result;
+	[...document.querySelectorAll("button")].forEach((b) => b.disabled = false);
+}
+const call_api = call("/api/");
+const call_api_path = call("/api/path");
+</script>
+<button onclick="call_api()">call /api/</button>
+<button onclick="call_api_path()">call /api/path</button>
+<p id="result"></p>
+EOF
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "OAI_policy" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  policy = "${data.aws_iam_policy_document.s3_policy.json}"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.OAI.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "OAI" {
 }
 
