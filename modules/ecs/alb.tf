ed661164f53f442e4a622799f3350564adccf64f
nana808-git <nana.devtestacct@gmail.com>
2021-03-15 09:38:58 -0500

    ok

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -255,27 +255,27 @@ resource "aws_s3_bucket" "bucket" {
   }  
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = "${aws_s3_bucket.bucket.bucket}"
-  key    = "index.html"
+#resource "aws_s3_bucket_object" "object" {
+#  bucket = "${aws_s3_bucket.bucket.bucket}"
+#  key    = "index.html"
 
-  content      = <<EOF
-<script>
-const call = (path) => async () => {
-	[...document.querySelectorAll("button")].forEach((b) => b.disabled = true);
-	const result = await (await fetch(path)).text();
-	document.querySelector("#result").innerText = result;
-	[...document.querySelectorAll("button")].forEach((b) => b.disabled = false);
-}
-const call_api = call("/api/");
-const call_api_path = call("/api/path");
-</script>
-<button onclick="call_api()">call /api/</button>
-<button onclick="call_api_path()">call /api/path</button>
-<p id="result"></p>
-EOF
-  content_type = "text/html"
-}
+#  content      = <<EOF
+#<script>
+#const call = (path) => async () => {
+#	[...document.querySelectorAll("button")].forEach((b) => b.disabled = true);
+#	const result = await (await fetch(path)).text();
+#	document.querySelector("#result").innerText = result;
+#	[...document.querySelectorAll("button")].forEach((b) => b.disabled = false);
+#}
+#const call_api = call("/api/");
+#const call_api_path = call("/api/path");
+#</script>
+#<button onclick="call_api()">call /api/</button>
+#<button onclick="call_api_path()">call /api/path</button>
+#<p id="result"></p>
+#EOF
+#  content_type = "text/html"
+#}
 
 resource "aws_s3_bucket_policy" "OAI_policy" {
   bucket = "${aws_s3_bucket.bucket.id}"
