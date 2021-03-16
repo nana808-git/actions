51e027633cf8a1727d9eaf4ff12ee5a9f482b48b
nana808-git <nana.devtestacct@gmail.com>
2021-03-16 14:02:28 -0500

    ok

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -217,9 +217,9 @@ resource "aws_cloudfront_distribution" "distribution" {
     #cloudfront_default_certificate = true
     acm_certificate_arn            = "var.ssl_certificate_arn"
     #iam_certificate_id             = "var.ssl_certificate_id"
-    cloudfront_default_certificate = var.ssl_certificate_arn == null && var.ssl_certificate_id == null ? true : false
-    ssl_support_method             = var.ssl_certificate_arn == null && var.ssl_certificate_id == null ? null : "sni-only"
-    minimum_protocol_version       = var.ssl_certificate_arn == null && var.ssl_certificate_id == null ? "TLSv1" : "TLSv1"
+    #cloudfront_default_certificate = var.ssl_certificate_arn == null && var.ssl_certificate_id == null ? true : false
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.1_2016"
   }
 }
 
