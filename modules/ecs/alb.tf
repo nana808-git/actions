8b2c85f5c01c9fcb98d253388b21509f00c16b0a
nana808-git <nana.devtestacct@gmail.com>
2021-03-16 20:32:10 -0500

    ok

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -280,7 +280,7 @@ resource "aws_s3_bucket_policy" "OAI_policy" {
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    actions   = ["s3:GetObject", "s3:PutObject"]
+    actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.bucket.arn}/*"]
 
     principals {
