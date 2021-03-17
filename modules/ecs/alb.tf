d5bb9577509224b5e6ba82da3b85f5af133f84ba
nana808-git <nana.devtestacct@gmail.com>
2021-03-17 07:29:37 -0500

    ok

--- modules/ecs/alb.tf
+++ modules/ecs/alb.tf
@@ -162,8 +162,8 @@ resource "aws_cloudfront_distribution" "distribution" {
 
     custom_origin_config {
       http_port              = 80
-#      https_port             = 443
-      origin_protocol_policy = "http-only"
+      https_port             = 443
+      origin_protocol_policy = "https-only"
 #      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"]
     }
   }
