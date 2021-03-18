18a1b63d3b0b44ec5aef26369aff482e57368d20
nana808-git <nana.devtestacct@gmail.com>
2021-03-18 14:24:16 -0500

    ok

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
-  value       = var.vpc_id
+  value       = "aws_vpc.main.id"
   description = "vpc id."
 }
 
 output "vpc_public_subnet_ids" {
-  value       = var.public_subnets
+  value       = "aws_subnet.public.*.id"
   description = "List of IDs of VPC public subnets."
 }
 
 output "vpc_private_subnet_ids" {
-  value       = var.private_subnets
+  value       = "aws_subnet.private.*.id"
   description = "List of IDs of VPC private subnets."
 }
