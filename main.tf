473495b5dd12c1a78d61afd0c75977988f97a2d4
nana808-git <nana.devtestacct@gmail.com>
2021-03-21 17:59:46 -0500

    ok

--- main.tf
+++ main.tf
@@ -22,6 +22,56 @@ module "pipeline" {
   subnet_ids                     = var.public_subnets
 }
 
+module "rds" {
+  source = "./modules/rds"
+  db_instance_type               = var.db_instance_type
+  db_name                        = var.db_name
+  db_user                        = var.db_user
+  db_port                        = var.db_port
+  db_password                    = var.db_password
+  db_profile                     = var.db_profile
+  db_initialize                  = var.db_initialize
+  db_engine                      = var.db_engine
+  db_version                     = var.db_version
+  db_allocated_storage           = var.db_allocated_storage 
+  cluster_name                   = var.cluster_name
+  environment                    = var.environment
+  vpc_id                         = var.vpc_id
+  cidr                           = var.cidr
+  subnet_ids                     = var.public_subnets
+  availability_zones             = var.availability_zones
+}
 
+module "ecs" {
+  source              = "./modules/ecs"
+  vpc_id              = var.vpc_id
+  cluster_name        = var.cluster_name
+  environment         = var.environment
+  #env_name            = var.env_name
+  image               = var.image
+  region              = var.region
+  repository_url      = module.ecs.repository_url
+  container_name      = var.container_name
+  app_repository_name = var.app_repository_name
+  repository_name     = var.repository_name
+  alb_port            = var.alb_port
+  container_port      = var.container_port
+  min_tasks           = var.min_tasks
+  max_tasks           = var.max_tasks
+  cpu_to_scale_up     = var.cpu_to_scale_up
+  cpu_to_scale_down   = var.cpu_to_scale_down
+  desired_tasks       = var.desired_tasks
+  desired_task_cpu    = var.desired_task_cpu
+  desired_task_memory = var.desired_task_memory
+
+  helth_check_path      = var.helth_check_path
+  environment_variables = var.environment_variables
+  ssl_certificate_arn   = var.ssl_certificate_arn
+  domain_name           = var.domain_name
+
+  availability_zones    = var.public_subnets
+  #availability_zones = "module.vpc.aws_subnet.public*.id"
+  #availability_zones = "aws_subnet.public.cidr_block"
+}
 
 
