8ba84efc1bdb595fbb6adbd706ba912ff0280257
nana808-git <nana.devtestacct@gmail.com>
2021-03-21 17:57:37 -0500

    ok

--- main.tf
+++ main.tf
@@ -23,36 +23,5 @@ module "pipeline" {
 }
 
 
-module "ecs" {
-  source              = "./modules/ecs"
-  vpc_id              = var.vpc_id
-  cluster_name        = var.cluster_name
-  environment         = var.environment
-  #env_name            = var.env_name
-  image               = var.image
-  region              = var.region
-  repository_url      = module.ecs.repository_url
-  container_name      = var.container_name
-  app_repository_name = var.app_repository_name
-  repository_name     = var.repository_name
-  alb_port            = var.alb_port
-  container_port      = var.container_port
-  min_tasks           = var.min_tasks
-  max_tasks           = var.max_tasks
-  cpu_to_scale_up     = var.cpu_to_scale_up
-  cpu_to_scale_down   = var.cpu_to_scale_down
-  desired_tasks       = var.desired_tasks
-  desired_task_cpu    = var.desired_task_cpu
-  desired_task_memory = var.desired_task_memory
-
-  helth_check_path      = var.helth_check_path
-  environment_variables = var.environment_variables
-  ssl_certificate_arn   = var.ssl_certificate_arn
-  domain_name           = var.domain_name
-
-  availability_zones    = var.public_subnets
-  #availability_zones = "module.vpc.aws_subnet.public*.id"
-  #availability_zones = "aws_subnet.public.cidr_block"
-}
 
 
