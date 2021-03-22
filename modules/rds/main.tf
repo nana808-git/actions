8c23ff0fe9995be548a2e41ba653463db83efeed
nana808-git <nana.devtestacct@gmail.com>
2021-03-22 09:55:23 -0500

    ok

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,9 +1,9 @@
 data "aws_subnet" "subnet1" {
-  id = "module.vpc.aws_subnet.private[0].id"
+  id = "module.vpc.private_subnet_ids"
 }
 
 data "aws_subnet" "subnet2" {
-  id = "module.vpc.aws_subnet.private[1].id"
+  id = "module.vpc.private[1]"
 }
 
 resource "aws_db_subnet_group" "default" {
