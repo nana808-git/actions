d154437e06fc0250b3baa018e040d8a66a5cba7f
nana808-git <nana.devtestacct@gmail.com>
2021-03-23 08:07:17 -0500

    ok

--- modules/pipeline/codepipeline.tf
+++ modules/pipeline/codepipeline.tf
@@ -13,34 +13,33 @@ resource "aws_codepipeline" "pipeline" {
   stage {
     name = "Source"
     action {
-      [
-        name = "Image"
-        category = "Source"
-        owner = "AWS"
-        provider = "ECR"
-        version = "1"
-        run_order = "0"
-        output_artifacts = ["source"]
-        configuration = {
-          RepositoryName = "${var.cluster_name}-${var.environment}-ecr-node"
-          ImageTag       = "latest"
-        }
-      ],
-      [
-        name = "GitHub"
-        category = "Source"
-        owner = "AWS"
-        provider = "CodeStarSourceConnection"
-        version = "1"
-        run_order = "1"
-        output_artifacts = ["source"]
-        configuration = {
-          FullRepositoryId = "${lookup(var.git_repository,"FullRepositoryId")}"
-          BranchName   = "${lookup(var.git_repository,"BranchName")}"
-          ConnectionArn = "${lookup(var.git_repository,"ConnectionArn")}"
-          OutputArtifactFormat = "CODE_ZIP"
-        }
-      ]
+      {
+      name = "Image"
+      category = "Source"
+      owner = "AWS"
+      provider = "ECR"
+      version = "1"
+      run_order = "0"
+      output_artifacts = ["source"]
+      configuration = {
+        RepositoryName = "${var.cluster_name}-${var.environment}-ecr-node"
+        ImageTag       = "latest"
+      }
+    },
+    {
+      name = "GitHub"
+      category = "Source"
+      owner = "AWS"
+      provider = "CodeStarSourceConnection"
+      version = "1"
+      run_order = "1"
+      output_artifacts = ["source"]
+      configuration = {
+        FullRepositoryId = "${lookup(var.git_repository,"FullRepositoryId")}"
+        BranchName   = "${lookup(var.git_repository,"BranchName")}"
+        ConnectionArn = "${lookup(var.git_repository,"ConnectionArn")}"
+        OutputArtifactFormat = "CODE_ZIP"
+      }
     }
   }
 
