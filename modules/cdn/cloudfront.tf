b8d587a4d71b1930b603cb2597ad22112fb71efe
nana808-git <nana.devtestacct@gmail.com>
2021-03-26 11:34:15 -0500

    ok

--- modules/cdn/cloudfront.tf
+++ modules/cdn/cloudfront.tf
@@ -22,8 +22,9 @@ resource "aws_cloudfront_distribution" "distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "allow all"
+      origin_protocol_policy = "match-viewer"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"]
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.OAI.cloudfront_access_identity_path}"
     }
   }
 
