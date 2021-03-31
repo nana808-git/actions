8d40f15e0757e4c2ed8cd0f1844b8870d8b25d14
nana808-git <nana.devtestacct@gmail.com>
2021-03-31 07:38:25 -0500

    build

--- terraform/environments/Dev/main.tf
+++ terraform/environments/Dev/main.tf
@@ -22,7 +22,6 @@ module "ecs-pipeline" {
   cidr            = ["${var.network["cidr"]}"]
   azs             = var.availability_zones
   region          = var.region
-  target_arns         = module.ecs.lb_arn
 
   cluster_name        = "${var.app["name"]}"
   app_repository_name = "${var.app["name"]}"
