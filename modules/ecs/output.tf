b0b37b8daee0d92d6822c8e047a3f083cd1a7708
nana808-git <nana.devtestacct@gmail.com>
2021-03-31 06:39:22 -0500

    build

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -10,17 +10,17 @@ output "service_name" {
   value = aws_ecs_service.web-api.name
 }
 
-output "app_sg_id" {
-  value = aws_security_group.app_sg.id
+#output "app_sg_id" {
+##  value = aws_security_group.app_sg.id
 }
 
-output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
-}
+#output "alb_sg_id" {
+#  value = aws_security_group.alb_sg.id
+#}
 
-output "ecs_sg_id" {
-  value = aws_security_group.ecs_sg.id
-}
+#output "ecs_sg_id" {
+#  value = aws_security_group.ecs_sg.id
+#}
 
 output "cloudwatch_log_group_arn" {
   value = aws_cloudwatch_log_group.web-app.arn
@@ -34,8 +34,8 @@ output "enable_ssl" {
   value = local.can_ssl
 }
 
-output "alb_dns_name" {
-  value = aws_alb.app_alb.dns_name
+output "lb_dns_name" {
+  value = aws_alb.app_nlb.dns_name
 }
 
 output "ecs_cluster_arn" {
