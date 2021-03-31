00eace024867d9a30bf6552c17174ba3ee53835a
nana808-git <nana.devtestacct@gmail.com>
2021-03-31 06:40:42 -0500

    build

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -11,8 +11,8 @@ output "service_name" {
 }
 
 #output "app_sg_id" {
-##  value = aws_security_group.app_sg.id
-}
+#  value = aws_security_group.app_sg.id
+#}
 
 #output "alb_sg_id" {
 #  value = aws_security_group.alb_sg.id
