6c826ccb9a8e82f215931af996f01ee29b3503a8
nana808-git <nana.devtestacct@gmail.com>
2021-03-31 06:53:57 -0500

    build

--- modules/ecs/services.tf
+++ modules/ecs/services.tf
@@ -19,24 +19,41 @@ resource "aws_ecs_service" "web-api" {
   //  }
 
   network_configuration {
-    security_groups  = local.security_group_ids
+    #security_groups  = local.security_group_ids
+    security_groups = [aws_security_group.nlb_sg.id]
     subnets          = var.availability_zones
     assign_public_ip = true // false
   }
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.api_target_group.arn
+    #target_group_arn = aws_alb_target_group.api_target_group.arn
+    target_group_arn = aws_lb_target_group.api_target_group.id
     container_name   = "${var.cluster_name}-${var.environment}-node-api"
     container_port   = var.container_port
   }
 
   lifecycle {
     ignore_changes = [
-      # task_definition,
-      load_balancer,
+      task_definition,
+      #load_balancer,
     ]
   }
 
-  depends_on = [aws_iam_role_policy.ecs_service_role_policy, aws_alb_target_group.api_target_group]
+  #depends_on = [aws_iam_role_policy.ecs_service_role_policy, aws_alb_target_group.api_target_group]
+  depends_on = [aws_lb_listener.web_app]
 }
+
+
+
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.replicas
+
+  network_configuration {
+    security_groups = [aws_security_group.nsg_task.id]
+    subnets         = split(",", var.private_subnets)
+  }
+
+
+
+
 
