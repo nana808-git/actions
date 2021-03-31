495f8f7549ae3168935d964950d8f8f7000ff9b8
nana808-git <nana.devtestacct@gmail.com>
2021-03-31 08:57:45 -0500

    build

--- modules/cdn/cloudfront.tf
+++ modules/cdn/cloudfront.tf
@@ -14,18 +14,18 @@ resource "aws_cloudfront_distribution" "distribution" {
     }
   }
 
-  origin {
-    domain_name = "${var.alb_dns_name}"
+#  origin {
+#    domain_name = "${var.alb_dns_name}"
   
-    origin_id   = "ELB"
+#    origin_id   = "ELB"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "match-viewer"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"]
-    }
-  }
+#    custom_origin_config {
+#      http_port              = 80
+#      https_port             = 443
+#      origin_protocol_policy = "match-viewer"
+#      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"]
+#    }
+#  }
 
   enabled             = true
   default_root_object = "index.html"
@@ -47,10 +47,10 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 
   ordered_cache_behavior {
-    path_pattern     = "/api/*"
+  #  path_pattern     = "/api/*"
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "ELB"
+    target_origin_id = "s3"
 
     default_ttl = 0
     min_ttl     = 0
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "distribution" {
     forwarded_values {
       query_string = true
       cookies {
-        forward = "all"
+        forward = "none"
       }
     }
 
@@ -72,12 +72,12 @@ resource "aws_cloudfront_distribution" "distribution" {
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-    #acm_certificate_arn            = "${var.ssl_cert}"
-    #ssl_support_method             = "sni-only"
-    #minimum_protocol_version       = "TLSv1.1_2016"
-  }
+#  viewer_certificate {
+#    cloudfront_default_certificate = true
+#    #acm_certificate_arn            = "${var.ssl_cert}"
+#    #ssl_support_method             = "sni-only"
+#    #minimum_protocol_version       = "TLSv1.1_2016"
+#  }
 }
 
 # Creates the DNS record to point on the CloudFront distribution ID that handles the redirection website
