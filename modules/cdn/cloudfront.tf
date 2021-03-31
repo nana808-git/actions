9a74f6f33b58032e2b9b94a034908df73556d486
nana808-git <nana.devtestacct@gmail.com>
2021-03-30 20:56:59 -0500

    build

--- modules/cdn/cloudfront.tf
+++ modules/cdn/cloudfront.tf
@@ -74,7 +74,7 @@ resource "aws_cloudfront_distribution" "distribution" {
 
   viewer_certificate {
     #cloudfront_default_certificate = true
-    acm_certificate_arn            = "${var.ssl_certificate_arn}"
+    acm_certificate_arn            = "${var.ssl_cert}"
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.1_2016"
   }
