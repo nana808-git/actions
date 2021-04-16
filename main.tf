4be137a5436723eda31b75b8ce7a9cd7ed52f39d
nana808-git <nana.devtestacct@gmail.com>
2021-04-16 10:32:15 -0500

    ok

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "pipeline" {
   codestar_connector_credentials = var.codestar_connector_credentials
   container_name                 = var.container_name
   app_repository_name            = var.app_repository_name
-  repository_name                = var.repository_name
+  #repository_name                = var.repository_name
   git_repository                 = var.git_repository
   repository_url                 = module.ecs.repository_url
   repository_name                = module.ecs.repository_name
