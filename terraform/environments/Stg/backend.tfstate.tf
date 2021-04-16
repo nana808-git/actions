0acc8660204daeb201b1b9498a464151824633a0
nana808-git <nana.devtestacct@gmail.com>
2021-04-16 08:16:42 -0500

    ok

--- terraform/environments/Stg/backend.tfstate.tf
+++ terraform/environments/Stg/backend.tfstate.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "infra-tf-backend"
-    key    = "ss/stg/terraform/terraform.tfstate"
+    key    = "kk/stg/terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
