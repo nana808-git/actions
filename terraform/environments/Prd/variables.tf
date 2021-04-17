5357cbaf192f7a4d44790c2034bc60c551f22d6a
nana808-git <nana.devtestacct@gmail.com>
2021-04-17 12:37:14 -0500

    ok

--- terraform/environments/Prd/variables.tf
+++ terraform/environments/Prd/variables.tf
@@ -30,24 +30,20 @@ variable "network" {
   }
 }
 
+
 variable "certificate_arn" {
   type = string
-  default = "arn:aws:iam::710789462061:server-certificate/Star_dtmediagrp.com"
-}
-
-variable "cloudfront_certificate_id" {
-  type = string
-  default = "ASCA2K7S2PAWRMDVOM4LW"
+  default = "arn:aws:acm:us-east-2:667736119737:certificate/06695160-eb02-4be0-96d5-e1d86e50847c"
 }
 
 variable "domain" {
   type = string
-  default = "dtmediagrp.com"
+  default = "nana808test.com"
 }
 
 variable "ssl_cert" {
   type = string
-  default = "*.dtmediagrp.com"
+  default = "nana808test.com"
 }
 
 variable "node_volume_size" {
@@ -83,4 +79,71 @@ variable "escluster_instance_count" {
 variable "escluster_instance_type" {
   type = string
   default = "t2.medium.elasticsearch"
+}
+
+variable "build_options" {
+  type        = string
+  default     = ""
+  description = "Docker build options. ex: '-f ./build/Dockerfile' "
+}
+
+variable "build_args" {
+  description = "docker build args."
+  type        = map(string)
+  default     = {}
+}
+
+variable "image" {
+  description = "The container image"
+  type        = string
+  default     = "module.ecs.image"
+}
+
+variable "repository_name" {
+  description = "Full name of ECR Repository"
+  default     = "module.ecs.repository_name"
+}
+
+variable "container_name" {
+  type        = string
+  description = "container app name"
+  default     = "module.ecs.container_name"
+}
+
+variable "s3-bucket" {
+  type        = string
+  description = "staging s3-bucket name"
+  default     = "module.cdn.s3-bucket"
+}
+
+
+variable "db_endpoint" {
+  description = "RDS Host name"
+  default     = "module.rds.db_endpoint"
+}
+
+variable "repository_url" {
+  description = "The url of the ECR repository"
+  default     = "module.ecs.repository_url"
+}
+
+variable "app_service_name" {
+  description = "Service name"
+  default     = "module.ecs.app_service_name"
+}
+
+variable "alb_port" {
+  type        = string
+  description = "origin application load balancer port"
+  default     = "module.ecs.alb_port"
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  description = "ecs task environment variables"
+
+  default = {
+    SQL_DB_NAME = "sleestak",
+    SQL_PORT = "3306",
+  }
 }
