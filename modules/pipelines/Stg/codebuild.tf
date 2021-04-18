043d27c0e664c1a638d75385c45ba8f815031aa8
nana808-git <nana.devtestacct@gmail.com>
2021-04-18 14:47:58 -0500

    ok

--- modules/pipelines/Stg/codebuild.tf
+++ modules/pipelines/Stg/codebuild.tf
@@ -20,6 +20,7 @@ data "template_file" "serverspec" {
     repository_url            = var.repository_url
     repository_name           = var.repository_name
     region                    = var.region
+    prd_region                = var.prd_region
     environment               = var.environment
     cluster_name              = var.cluster_name
     container_name            = var.container_name
@@ -114,6 +115,7 @@ data "template_file" "clientspec" {
     repository_url            = var.repository_url
     repository_name           = var.repository_name
     region                    = var.region
+    prd_region                = var.prd_region
     environment               = var.environment
     cluster_name              = var.cluster_name
     container_name            = var.container_name
@@ -208,6 +210,7 @@ data "template_file" "dbspec" {
     repository_url            = var.repository_url
     repository_name           = var.repository_name
     region                    = var.region
+    prd_region                = var.prd_region
     environment               = var.environment
     cluster_name              = var.cluster_name
     container_name            = var.container_name
